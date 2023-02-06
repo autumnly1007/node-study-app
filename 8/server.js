7f0023179a75dba4fb4bfef135009c64e5d57ca0
autumnly1007 <autumnly1007@gmail.com>
2023-02-06 21:51:37 +0900

    Add Graphql

--- 8/server.js
+++ 8/server.js
@@ -1,26 +1,46 @@
+const { loadFilesSync } = require('@graphql-tools/load-files');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
+const path = require('path');
 
 const app = express();
 
-const schema = buildSchema(`
+const schemaString = `
   type Query {
-    description: String
-    name: String
+    posts: [Post]
+    comments: [Comment]
   }
-`);
+  
+  type Post {
+    id: ID!
+    title: String!
+    description: String!
+    comments: [Comment]
+  }
+  
+  type Comment {
+    id: ID!
+    text: String!
+    likes: Int
+  }
+`;
 
-const root = {
-  description: 'Hello World',
-  name: 'Cat',
-};
+const loadedTypes = loadFilesSync('**/*', {
+  extensions: ['graphql'],
+});
+
+const loadedResolvers = loadFilesSync(path.join(__dirname, '**/*.resolvers.js'));
+
+const schema = makeExecutableSchema({
+  typeDefs: [loadedTypes],
+  resolvers: loadedResolvers,
+});
 
 app.use(
   '/graphql',
   graphqlHTTP({
     schema: schema,
-    rootValue: root,
     graphiql: true,
   })
 );
